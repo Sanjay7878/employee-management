0cc53940d7ba40e1e896226550a09fc4c533570c
sanjay7878 <sanjay@pickright.in>
2024-08-24 15:33:15 +0530

    side nav component created

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { SideNavComponent } from './side-nav/side-nav.component';
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    SideNavComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
