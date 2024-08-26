2107bb252700421b053a56ca66622a22cdee612a
sanjay7878 <sanjay@pickright.in>
2024-08-26 21:04:16 +0530

    add employee design implementation

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.scss'
 })
 export class AddEmployeeComponent {
 
+  allDesignation: string[] = ['Software Developer', 'Senior Software Developer', 'Quality Assurance',
+    'Technical Lead', 'Manager']
+  showDesingation: boolean = false;
+  showAvatar: boolean = false;
+  allAvatars: string[] = ['assets/profiles/profile-1.png', 'assets/profiles/profile-2.png','assets/profiles/profile-3.png', 'assets/profiles/profile-4.png']
 }
